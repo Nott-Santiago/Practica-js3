4a55db8ef45291eb0ed90e8bd22f8978c18154d6
Santiago <18030081@itcelaya.edu.mx>
2021-12-14 20:36:51 -0600

    Commit 4

--- js/view.js
+++ js/view.js
@@ -1,13 +1,16 @@
 import AddTodo from './components/add-todo.js';
+import Modal from './components/modal.js';
 
 export default class view{
     constructor(){
         this.model = null;
         this.table = document.getElementById('table');
         this.addTodoForm = new AddTodo();
+        this.modal = new Modal();
+
 
         this.addTodoForm.onClick((title, description) => this.addTodo(title, description));
-    
+        this.modal.onClick((id, values) => this.editTodo(id, values));
     }
 
     setModel(model){
@@ -28,6 +31,14 @@ export default class view{
         this.model.toggleCompleted(id);
     }
 
+    editTodo(id, values){
+        this.model.editTodo(id, values);
+        const row = document.getElementById(id);
+        row.children[0].innerText = values.title;
+        row.children[1].innerText = values.description;
+        row.children[2].children[0].checked = values.completed;
+    }
+
     removeTodo(id){
         this.model.removeTodo(id);
         document.getElementById(id).remove();
@@ -43,9 +54,7 @@ export default class view{
                             
                         </td>
                         <td class="text-right">
-                            <button class="btn btn-primary mb-1">
-                            <i class="fa fa-pencil"></i>
-                            </button>
+                            
                         </td> 
                     `;
 
@@ -54,7 +63,15 @@ export default class view{
         checkbox.checked = todo.completed;
         checkbox.onclick = () => this.toggleCompleted(todo.id);
         row.children[2].appendChild(checkbox);
-    
+
+        const editBtn = document.createElement('button');
+        editBtn.classList.add('btn','btn-primary', 'mb-1');
+        editBtn.innerHTML = '<i class="fa fa-pencil"></i>';
+        editBtn.setAttribute('data-toggle', 'modal');
+        editBtn.setAttribute('data-target', '#modal');
+        editBtn.onclick = () => this.modal.setValues(todo);
+        row.children[3].appendChild(editBtn);
+
         const removeBtn = document.createElement('button');
         removeBtn.classList.add('btn','btn-danger', 'mb-1', 'ml-1');
         removeBtn.innerHTML = '<i class="fa fa-trash"></i>';
